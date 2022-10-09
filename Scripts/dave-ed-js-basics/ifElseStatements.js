6d3f1de55ffc5e7cb2285160ea476ccf5a88e07e
Stanislav Dmytriyev <stanislav.dmytriyev@gmail.com>
2022-10-09 23:02:32 +0300

    testing integrated visual studio commit

---
+++ Scripts/dave-ed-js-basics/ifElseStatements.js
@@ -0,0 +1,10 @@
+const myAge = 35;
+
+// то есть если myAge > 18, то выполняется console.log("You are good to go!")
+if (myAge > 18) {
+  console.log("You are good to go!");
+}
+// а если myAge < или равен 18, то выполняется другое условие (else)
+else {
+  console.log("You are too young for a bear :(");
+}
--- Scripts/dave-ed-js-basics/stringConcatenation.js
+++ Scripts/dave-ed-js-basics/stringConcatenation.js
@@ -2,9 +2,27 @@ console.log("-=-=-=-START OF STRING CONCATENATION SCRIPT / LESSON-=-=-=-");
 // СЛЕПИЛИ 2 СТРИНГА, ВСЕ ОК
 console.log("Hello, my name is " + "Stanislav");
 // ЗАХОТЕЛИ ТЕКСТ С апострофом - и оно приняло ее за закрывающую кавычку
+// чтобы js не принял простой апостроф за закрывающую - перед ним ставим слэш
+// вот так вот получается \'
 // Кстати, PRETTIER заменяет одинарные кавычки на двойные
 // С двойными, в принципе, не бывает никаких вообще проблем
 // Это более новый и современный метод (раньше писали одинарными)
-console.log('Hello it's me Stanislav');
+console.log("Hello it's me Stanislav");
 
+// А теперь актуальный и крутой метод с BACKTICKS
+const nameee = "Stani Dmitrssen";
+const age = 35;
+// то есть никаких плюс и прочего - сразу текст и переменная
+console.log(`Hello it's me ${nameee}`);
+console.log(`Hello it's me ${123125435464536}`);
+// пример неудобного синтаксиса (старого)
+console.log(
+  "hello it's me " + nameee + " and my age is " + age + " years old."
+);
+// пример крутого синтаксиса (нового), тут AGE превратится в стринг
+console.log(`hello it's me ${nameee} and my age is ${age} years old.`);
+// проверим, как число превращается в стринг при конкатинации
+const combined = nameee + age;
+console.log(combined);
+console.log(typeof combined);
 console.log("-=-=-=-END OF STRING CONCATENATION SCRIPT / LESSON-=-=-=-");
