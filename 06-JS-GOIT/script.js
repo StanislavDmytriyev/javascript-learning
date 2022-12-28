b7d7b183b44b396b81c492203c8cb3b0ee41b117
Stanislav Dmytriyev <stanislav.dmytriyev@gmail.com>
2022-12-28 14:54:47 +0200

    Learning JS basics

---
+++ 06-JS-GOIT/script.js
@@ -0,0 +1,85 @@
+function checkStorage(available, ordered) {
+  let message;
+  message =
+    ordered > available
+      ? "Not enough goods in stock!"
+      : "The order is accepted, our manager will contact you";
+  return message;
+}
+
+function getSubscriptionPrice(type) {
+  let price;
+  switch (type) {
+    case "starter":
+      price = 0;
+      break;
+    case "professional":
+      price = 20;
+      break;
+    case "organization":
+      price = 50;
+      break;
+  }
+}
+
+function checkPassword(password) {
+  const ADMIN_PASSWORD = "jqueryismyjam";
+  let message;
+  switch (password) {
+    case null:
+      message = "Canceled by user!";
+      break;
+    case "jqueryismyjam":
+      message = "Welcome!";
+      break;
+    default:
+      message = "Access denied, wrong password!";
+  }
+  return message;
+}
+
+function getShippingCost(country) {
+  let message;
+  switch (country) {
+    case "China":
+      message = `Shipping to ${country} will cost 100 credits`;
+      break;
+    case "Chile":
+      message = `Shipping to ${country} will cost 250 credits`;
+      break;
+    case "Australia":
+      message = `Shipping to ${country} will cost 170 credits`;
+      break;
+    case "Jamaica":
+      message = `Shipping to ${country} will cost 120 credits`;
+      break;
+    default:
+      message = "Sorry, there is no delivery to your country";
+  }
+  return message;
+}
+
+function formatMessage(message, maxLength) {
+  let result;
+  if (message.length <= maxLength) {
+    result = message;
+  } else {
+    result = message.slice(0, maxLength);
+  }
+  return result;
+}
+
+function formatMessage(message, maxLength) {
+  let result;
+  if (message.length <= maxLength) {
+    result = message;
+  } else {
+    result = message.slice(0, maxLength) + "...";
+  }
+  return result;
+}
+
+function normalizeInput(input) {
+  const normalizedInput = input.toLowerCase();
+  return normalizedInput;
+}
